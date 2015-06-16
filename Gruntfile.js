7b76e7f217f478f83c0bc898cadec414847254ba
Patrick Gazaneo <pck1993@gmail.com>
2015-06-16 18:50:57 -0300

    Adição do typescript

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,59 @@
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
-    var config = {
-        app: 'app'
-    };
-    grunt.initConfig({
-        appConfig: config,
-        watch: {
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= appConfig.app %>/*.html',
-                    '<%= appConfig.app %>/styles/*.css',
-                    '<%= appConfig.app %>/*.js',
-                    '<%= appConfig.app %>/img/*.{png,jpg,jpeg,gif,webp,svg}'
-                ]
-            }
+  require('load-grunt-tasks')(grunt);
+  var config = {
+    app: 'app'
+  };
+  grunt.initConfig({
+    appConfig: config,
+    watch: {
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
         },
-        wiredep: {
-            app: {
-                src: ['<%= appConfig.app %>/index.html'],
-                ignorePath: /\.\.\//
-            }
-        },
-        connect: {
-            options: {
-                port: 9000,
-                // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost',
-                livereload: 35729
-            },
-            server: {
-                options: {
-                    open: true,
-                    middleware: function(connect) {
-                        return [
-                            connect().use(
-                                '/bower_components',
-                                connect.static('./bower_components')
-                            ),
-                            connect.static(config.app)
-                        ];
-                    }
-                }
-            }
+        files: [
+          '<%= appConfig.app %>/*.html',
+          '<%= appConfig.app %>/styles/*.css',
+          '<%= appConfig.app %>/*.js',
+          '<%= appConfig.app %>/img/*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+    wiredep: {
+      app: {
+        src: ['<%= appConfig.app %>/index.html'],
+        ignorePath: /\.\.\//
+      }
+    },
+    ts: {
+      default: {
+        src: ["<%= appConfig.app %>/*.ts", "!node_modules/**/*.ts", "typings/**/*.d.ts"],
+        options:{
+            sourceMap: false
         }
-    });
-    grunt.registerTask('server', ['wiredep','connect:server', 'watch']);
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      server: {
+        options: {
+          open: true,
+          middleware: function(connect) {
+            return [
+              connect().use(
+                '/bower_components',
+                connect.static('./bower_components')
+              ),
+              connect.static(config.app)
+            ];
+          }
+        }
+      }
+    }
+  });
+  grunt.registerTask('server', ['ts', 'wiredep', 'connect:server', 'watch']);
 };
