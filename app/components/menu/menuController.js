6db3150b478283b7834e9050c7c2d7ccb2f4a5ef
Patrick Gazaneo <pck1993@gmail.com>
2015-07-01 00:38:16 -0300

    Remoção de js

--- app/components/menu/menuController.js
+++
@@ -1,6 +0,0 @@
-angular.module('Template').controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
-        $scope.isOpen = true;
-        $scope.toggleSidenav = function (menuId) {
-            $scope.isOpen = !$scope.isOpen;
-        };
-    }]);
