34213fdbc56cc6611e336f8829e622bdc2a9fd5a
patrick.gazaneo <patrick.gazaneo@stoneage.com.br>
2015-08-05 17:03:10 -0300

    Validação no wizard

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,4 @@
-angular.module('Template', ['ngMaterial'])
+angular.module('Template', ['ngMaterial', 'ngMessages'])
     .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('blue', {
